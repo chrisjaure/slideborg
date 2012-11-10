9b46ef82a9064bc019b6a767d13ba3429f9fc0c3
Mathew Peterson <cpress@gmail.com>
2012-11-10 01:54:49 -0600

    Adds methods to broadcast to a room and inital setup to handle socket requests.

--- io.js
+++ io.js
@@ -7,6 +7,10 @@ var sio = require('socket.io'),
 
 exports.listen = function(server) {
 	io = sio.listen(server);
+
+	io.sockets.on('connection', function(socket) {
+
+	});
 };
 
 exports.createSession = function(url) {
@@ -17,4 +21,8 @@ exports.createSession = function(url) {
 
 exports.getSession = function(id) {
 	return sessions[id];
+};
+
+exports.broadcastToRoom = function (session, event, message) {
+	io.sockets.in(session.id).emit(event, message);
 };
